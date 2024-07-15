5b0794718e746cb6f6a1c16d7dc5bf5b8029f3b3
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-16 00:18:25 +0530

    port updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
+const port = process.env.PORT || 3000
 
-server.listen(3000, console.log("app is running"))
+server.listen(port, console.log("app is running"))
