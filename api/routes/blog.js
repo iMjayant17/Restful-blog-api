ec59da10560979cee4d11d512aeeb6ef507dc9f9
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-16 00:34:32 +0530

    updated

--- api/routes/blog.js
+++ api/routes/blog.js
@@ -4,7 +4,7 @@ const Blogg = require('../model/blogSchema')
 const mongoose = require('mongoose');
 const checkAuth = require('../middleware/checkAuth')
 
-router.get('/',checkAuth, (req, res, next) => {
+router.get('/', (req, res, next) => {
     res.status(200).json({
         msg:"completely working"
     })
